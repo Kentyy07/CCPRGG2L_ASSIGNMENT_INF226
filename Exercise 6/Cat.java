3d97d65e8183d40210465b11bc743f0de593e03c
Kent Nicholas L. Dela Cruz <120175283+Kentyy07@users.noreply.github.com>
2023-02-13 11:49:09 +0800

    Add files via upload

---
+++ Exercise 6/Cat.java
@@ -0,0 +1,6 @@
+public class Cat {
+    
+    void eat(){
+        System.out.println("Gutom na ako tas gusto ko na matulog pa uwi na ako haha");
+    }
+}
---
+++ Exercise 6/Pet.java
@@ -0,0 +1,8 @@
+ abstract class Pet {
+    
+    abstract void eat();
+
+    void sleep(){
+        System.out.println("uwi na ko zzzzzz");
+    }
+}
